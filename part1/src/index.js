9da66d26364fc3f3d723230cb40b976d058b8211
superRainbow <g901612002@hotmail.com>
2020-06-19 15:20:15 +0800

    add anecdotes

--- part1/src/index.js
+++ part1/src/index.js
@@ -38,9 +38,17 @@ const Statistics = (props) => {
 };
 
 const App = () => {
-  // const [ good, setGood] = useState(0);
-  // const [ neutral, setNeutral] = useState(0);
-  // const [ bad, setBad] = useState(0);
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ];
+
+  const [points, setPoints] = useState({ 0: 1, 1: 3, 2: 4, 3: 2 });
+  const [selected, setSelected] = useState(0);
   const [ data, setData] = useState({
     good: 0,
     neutral: 0,
@@ -53,13 +61,21 @@ const App = () => {
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button handleClick={() => clickEvent('good')} text="good" />
-      <Button handleClick={() => clickEvent('neutral')} text="neutral" />
-      <Button handleClick={() => clickEvent('bad')} text="bad" />
       <div>
-        <p>Statistics</p>
-        <Statistics data={data} />
+        <div>{anecdotes[selected]}</div>
+        <p>has {points[selected]} votes.</p>
+        <Button handleClick={() => setPoints({ ...points, [selected]: points[selected] + 1 })} text="vote" />
+        <Button handleClick={() => setSelected(selected + 1)} text="next anecdote" />
+      </div>
+      <div>
+        <h1>give feedback</h1>
+        <Button handleClick={() => clickEvent('good')} text="good" />
+        <Button handleClick={() => clickEvent('neutral')} text="neutral" />
+        <Button handleClick={() => clickEvent('bad')} text="bad" />
+        <div>
+          <p>Statistics</p>
+          <Statistics data={data} />
+        </div>
       </div>
     </div>
   )
