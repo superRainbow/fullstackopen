235df4923d4dced7c08b7cc4c18d23629d351174
superRainbow <g901612002@hotmail.com>
2020-06-29 11:59:53 +0800

    refactor：元件名稱

--- part2/src/components/content/index.js
+++ part2/src/components/content/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Part from '../part/part';
+import Part from '../part';
 
 const Content = ({data}) => (
   <ul>
--- part2/src/components/course/index.js
+++ part2/src/components/course/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import Header from './header/header';
-import Content from './content/content';
-import Total from './total/total';
+import Header from '../header';
+import Content from '../content';
+import Total from '../total';
 
 const Course = ({course}) => {
   const total = course.parts.reduce((accumulator, item) => accumulator + item.exercises, 0);
